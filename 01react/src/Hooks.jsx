5a5976d1de1adba00d033c3509ddac192cbf6462
Ihtesham Elahi <ihtesham.elahi.7@gmail.com>
2025-04-23 03:16:59 +0500

    Limits

--- 01react/src/Hooks.jsx
+++ 01react/src/Hooks.jsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
+
 function Hooks() {
-  let [counter, userCounter] = useState(7);
+  const [counter, setCounter] = useState(7);
+  const [message, setMessage] = useState("");
+
   const addValue = () => {
-    let newCounter = counter + 1;
-    userCounter(newCounter);
+    if (counter < 20) {
+      setCounter(counter + 1);
+      setMessage(""); // clear message if valid
+    } else {
+      setMessage("Value not allowed more than 20");
+    }
   };
 
   const removeValue = () => {
-    userCounter(counter - 1);
+    if (counter > 0) {
+      setCounter(counter - 1);
+      setMessage(""); // clear message if valid
+    } else {
+      setMessage("Value not allowed less than 0");
+    }
   };
+
   return (
     <>
       <button onClick={addValue}>Add Value {counter}</button>
       <button onClick={removeValue}>Remove Value {counter}</button>
+      <p>{message}</p>
     </>
   );
 }
+
 export default Hooks;
